38bbfcfdfa870ce3771fecb6a5ec5d961d07af18
s3r-b6 <103552324+s3r-b6@users.noreply.github.com>
2022-05-10 17:23:50 +0200

    Prototipo 2 - Aún no funciona
    Re-estilado (ahora usa flex) y prototipo de JS. Todavía no funciona el reajuste.

--- pizarramagica.js
+++ pizarramagica.js
@@ -1,8 +1,8 @@
 function etchASketch() {
   let wrapper = document.querySelector("#wrapper");
-  if (screen.height / screen.width < 16 / 9) {
-    for (columns = 1; columns < 17; columns++) {
-      for (row = 1; row < 17; row++) {
+  function createGrid(maxColumns, maxRows) {
+    for (columns = 1; columns < maxColumns + 1; columns++) {
+      for (row = 1; row < maxRows + 1; row++) {
         let gridrow = document.createElement("div");
         gridrow.classList.add("grid", "row");
         Object.assign(gridrow.style, {
@@ -10,25 +10,15 @@ function etchASketch() {
           gridRowEnd: row,
           gridColumnStart: columns,
           gridColumnEnd: columns,
-        });
-        wrapper.appendChild(gridrow);
-      }
-    }
-  } else if (screen.height / screen.width >= 16 / 9) {
-    for (columns = 1; columns < 27; columns++) {
-      for (row = 1; row < 17; row++) {
-        let gridrow = document.createElement("div");
-        gridrow.classList.add("grid", "row");
-        Object.assign(gridrow.style, {
-          gridRowStart: row,
-          gridRowEnd: row,
-          gridColumnStart: columns,
-          gridColumnEnd: columns,
+          /*width: calc(75vh/maxRows),
+          height: calc(75vh/maxRows),*/
         });
         wrapper.appendChild(gridrow);
       }
     }
   }
+  //inicialización por defecto
+  createGrid(16, 16);
 
   let hoveredGrid = document.querySelectorAll(".grid");
   [].forEach.call(hoveredGrid, function (el) {
@@ -44,5 +34,40 @@ function etchASketch() {
       el.classList.remove("hover");
     });
   });
+
+  //ajuste
+  document.querySelector(".adjust").addEventListener("click", () => {
+    document.querySelector("#adjust-pop").classList.remove("inv");
+  });
+
+  document.querySelector("#submit-inp").addEventListener("click", () => {
+    document.querySelector("#adjust-pop").classList.add("inv");
+  });
+
+  document.querySelector("#submit-inp").addEventListener("click", () => {
+    let numRows = document.getElementById("adjust-row").value;
+    let numCols = document.getElementById("adjust-col").value;
+    console.log(numRows, numCols);
+    if (numRows > 0 && numCols > 0 && numRows < 101 && numCols < 101) {
+      while (wrapper.firstChild) {
+        wrapper.removeChild(wrapper.lastChild);
+      }
+      createGrid(numRows, numCols);
+    } else {
+      prompt("Valor inválido! Rango aceptado [1-100]");
+    }
+  });
 }
 window.onload = etchASketch;
+
+/*
+<input id="name" type="text">
+<button onclick="go()">Go</button>
+And a simple script to go with it that is called when the button is clicked:
+
+function go() {
+    var text = document.getElementById("name").value;
+    alert("The user typed '" + text + "'");
+}
+
+*/
